0debfc1f5a123e8b92bf0d22ada5c88c6c29cc87
soumanpaul <souman1729@gmail.com>
2024-02-03 16:35:43 +0530

    asdd

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import './index.css';
+import { FaHeartBroken ,FaRegHeart, FaTimes } from "react-icons/fa";
+
+export default function App() {
+
+  const [noCount, setNoCount] = useState(0);
+
+  function handleClickNo() {
+    setNoCount((prevCount) => prevCount + 1);
+  }
+
+  function handleClickYes() {
+    setNoCount(0); // reset the count on affirmative answer
+    setIsExcited(true);
+  }
+
+  const phrases = [
+    "",
+    "Are you sure, No? ",
+    "Really sure, No?",
+    "Think again Souji ?",
+    "Last chance Souji ?",
+    "Surely not?",
+    "You might regret this Souji ?",
+    "Give it another thought Souji ?",
+    "Are you absolutely certain?",
+    "This could be a mistake Souji ?",
+    "Have a heart for me souji!",
+    "Don't be so cold Souji ?",
+    "Wouldn't you reconsider?",
+    "What is your final answer?",
+    "You're breaking my heart Souji;(",
+    "Now i am dead 💀"
+  ];
+
+  const getNoButtonText = () => {
+     return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  const [isExcited, setIsExcited] = useState(false);
+
+  return (
+    <div className="h-fit m-4 flex flex-col items-center justify-center p-8 rounded-2xl bg-cyan-500 shadow-2xl">
+      {isExcited ? (
+        <>
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">Good, You are my girl 🤗</h1>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="Excited bear"  />
+          <button
+          onClick={() => location.reload()}
+              className="bg-[#FF5A5F] text-white rounded-lg px-4 py-2 shadow-md hover:bg-[#ff3b54] transform hover:scale-105 transition-all duration-150 ease-out mt-8"
+          
+          >Start again</button>
+        </>
+      ) : (
+        <>
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">Souji will you be my valentine?</h1>
+          <p className="text-xl text-gray-600 mb-6">Choose wisely!</p>
+          <img  src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" alt="Bear with roses" className={`rounded-lg h-[150px]`} />
+
+        <div className="mt-6">
+          {getNoButtonText() != "" &&
+          <button
+              className="bg-gray-200 text-gray-900 rounded-lg px-6 py-2 shadow-md hover:bg-gray-300 transform hover:scale-105 transition-all duration-150 ease-out"
+            >
+              { getNoButtonText()} 
+            </button>
+            }
+        </div>
+
+          <div className="flex space-x-4 mt-10">
+            <button
+              id="yes-button"
+              onClick={handleClickYes}
+              className="bg-[#FF5A5F] text-white rounded-lg px-4 py-2 shadow-md hover:bg-[#ff3b54] transform hover:scale-105 transition-all duration-150 ease-out"
+            >
+              <span id="yes-text">Yes i will</span> <FaRegHeart className="h-6 w-6 inline ml-1" />
+            </button>
+            <button
+              onClick={handleClickNo}
+              className="bg-gray-200 text-gray-800 rounded-lg px-6 py-2 shadow-md hover:bg-gray-300 transform hover:scale-105 transition-all duration-150 ease-out"
+            >
+               No  <FaTimes className="h-6 w-3 inline ml-1" />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/components/C1.jsx
@@ -0,0 +1,70 @@
+export default function Component() {
+  const [noCount, setNoCount] = useState(0);
+
+  function handleClickNo() {
+    setNoCount((prevCount) => prevCount + 1);
+  }
+
+  function handleClickYes() {
+    setNoCount(0); // reset the count on affirmative answer
+    setIsExcited(true);
+  }
+
+  const phrases = [
+    "No",
+    "Are you sure?",
+    "Really sure?",
+    "Think again!",
+    "Last chance!",
+    "Surely not?",
+    "You might regret this!",
+    "Give it another thought!",
+    "Are you absolutely certain?",
+    "This could be a mistake!",
+    "Have a heart!",
+    "Don't be so cold!",
+    "Change of heart?",
+    "Wouldn't you reconsider?",
+    "Is that your final answer?",
+    "You're breaking my heart ;(",
+  ];
+
+  const getNoButtonText = () => {
+     return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  const [isExcited, setIsExcited] = useState(false);
+
+  return (
+    <div className="bg-white m-auto flex flex-col items-center justify-center p-8 rounded-2xl shadow-lg">
+      {isExcited ? (
+        <>
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">Good, I expect flowers 🤗</h1>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="Excited bear" />
+        </>
+      ) : (
+        <>
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">Will you be my valentine?</h1>
+          <p className="text-xl text-gray-600 mb-10">Choose wisely, I can't wait to hear from you!</p>
+          <img className="h-[200px]" src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" alt="Bear with roses" />
+
+          <div className="flex space-x-4">
+            <button
+              id="yes-button"
+              onClick={handleClickYes}
+              className="bg-[#FF5A5F] text-white rounded-lg px-6 py-2 shadow-md hover:bg-[#ff3b54] transform hover:scale-105 transition-all duration-150 ease-out"
+            >
+              <span id="yes-text">Yes</span> <IconHeart className="h-6 w-6 inline ml-1" />
+            </button>
+            <button
+              onClick={handleClickNo}
+              className="bg-gray-200 text-gray-800 rounded-lg px-6 py-2 shadow-md hover:bg-gray-300 transform hover:scale-105 transition-all duration-150 ease-out"
+            >
+              {getNoButtonText()} <IconX className="h-6 w-6 inline ml-1" />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
